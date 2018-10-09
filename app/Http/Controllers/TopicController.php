6757bb8512a2421586f36c6b808f0b718f6d4a8e
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-10-09 16:12:19 -0400

    05.1 Crear un topic, post
    despues de logearse. puede crear un topic pero al mismo tiempo crear un post con los datos a quien pertenece el post y de que topic proviene.

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Post;
+use App\Topic;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreTopicRequest;
 
@@ -9,6 +10,25 @@ class TopicController extends Controller
 {
     public function store(StoreTopicRequest $request)
     {
+        #====== Crear nueva instancia
+        $topic = new Topic;
+        #====== guardar el titulo del request
+        $topic->title = $request->title;
+        #====== asociar el User con el topic
+        #=== para eso accedemos al user relationship
+        #usamos el associate method y le pasamos el user que queremos asociar
+        $topic->user()->associate($request->user());
 
+        #=== save post in the topic
+        $post = new Post;
+        #====== guardar el body del request
+        $post->body = $request->body;
+        $post->user()->associate($request->user());
+
+        $topic->save();
+        #====== guardar el post a ese topic
+        $topic->posts()->save($post);
+
+        
     }
 }
