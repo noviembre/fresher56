564afd0f80f6d87e7329d5197f08df8b6df6b0ad
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-10-10 11:38:28 -0400

    09.1 Topic Update
    Creando el route y el Request

--- routes/api.php
+++ routes/api.php
@@ -32,5 +32,8 @@ Route::group(['prefix' => 'topics'], function () {
     #======== topic show
     Route::get('/{topic}', 'TopicController@show');
 
+    #======== topic update
+    Route::patch('/{topic}', 'TopicController@update')->middleware('auth:api');
+
 
 });
