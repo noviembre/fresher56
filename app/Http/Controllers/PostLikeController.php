03cbe48f152918cb590852f8f48d0bc0f2e2c555
Artemis Deepin 8GB <perudragon@hotmail.com>
2018-10-11 10:48:50 -0400

    15.2 Dar Likes a Post
    ahora al momento de darle like por asi decirlo (pq lo del like lo realizamos a traves de la url), en la db se guardan los datos de post y del user.

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -10,6 +10,12 @@ class PostLikeController extends Controller
 {
     public function store(Request $request, Topic $topic, Post $post)
     {
-        //
+
+        $like = new Like;
+        $like->user()->associate($request->user());
+
+        $post->likes()->save($like);
+
+        return response(null, 204);
     }
 }
